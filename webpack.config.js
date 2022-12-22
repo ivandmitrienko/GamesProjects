0007221f33322bd038a4ffc5173a949f2ac6b03a
ivandmitrienko <oneblackcrystal@gmail.com>
2022-12-22 23:35:23 +0300

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
                 use: 'babel-loader',
                 exclude: /node_modules/,
             },
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+            },
         ],
     },
     plugins: [
