3b15641257df4163332306238a2095195fafc790
ivandmitrienko <oneblackcrystal@gmail.com>
2022-12-24 14:22:35 +0300

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -8,51 +8,50 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'index.[contenthash:8].js',
-        // assetModuleFilename: path.join('images', '[name].[contenthash][ext]'),
-        assetModuleFilename: 'assets/images/[name][ext]'
+        assetModuleFilename: path.join('images', '[name].[contenthash][ext]'),
     },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.(scss|css)$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.svg$/,
-                type: 'asset/resource',
-                // generator: {
-                //     filename: path.join('icons', '[name].[contenthash][ext]'),
-                // },
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src', 'template.html'),
-            filename: 'index.html',
-        }),
-        new FileManagerPlugin({
-            events: {
-                onStart: {
-                    delete: ['dist'],
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    use: 'babel-loader',
+                    exclude: /node_modules/,
                 },
-            },
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash:8].css',
-        }),
-    ],
-    devServer: {
-        watchFiles: path.join(__dirname, 'src'),
-        port: 3000,
-    },
-};
+                {
+                    test: /\.(scss|css)$/,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+                },
+                {
+                    test: /\.(png|jpg|jpeg|gif)$/i,
+                    type: 'asset/resource',
+                },
+                {
+                    test: /\.svg$/,
+                    type: 'asset/resource',
+                    generator: {
+                        filename: path.join('icons', '[name].[contenthash][ext]'),
+                    },
+                },
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.join(__dirname, 'src', 'template.html'),
+                filename: 'index.html',
+            }),
+            new FileManagerPlugin({
+                events: {
+                    onStart: {
+                        delete: ['dist'],
+                    },
+                },
+            }),
+            new MiniCssExtractPlugin({
+                filename: '[name].[contenthash:8].css',
+            }),
+        ],
+        devServer: {
+            watchFiles: path.join(__dirname, 'src'),
+            port: 3000,
+        },
+    };
