cd11017ef1a1a981235f4afacca2a2f7d9f1e592
ivandmitrienko <oneblackcrystal@gmail.com>
2022-12-24 23:35:27 +0300

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -8,50 +8,54 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'index.[contenthash:8].js',
-        // assetModuleFilename: path.join('images', '[name].[contenthash][ext]'),
+        assetModuleFilename: path.join('images', '[name].[contenthash][ext]'),
     },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    use: 'babel-loader',
-                    exclude: /node_modules/,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.svg$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: path.join('icons', '[name].[contenthash][ext]'),
                 },
-                {
-                    test: /\.(scss|css)$/,
-                    use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
-                },
-                {
-                    test: /\.(png|jpg|jpeg|gif)$/i,
-                    type: 'asset/resource',
-                },
-                // {
-                //     test: /\.svg$/,
-                //     type: 'asset/resource',
-                //     generator: {
-                //         filename: path.join('icons', '[name].[contenthash][ext]'),
-                //     },
-                // },
-            ],
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: path.join(__dirname, 'src', 'template.html'),
-                filename: 'index.html',
-            }),
-            new FileManagerPlugin({
-                events: {
-                    onStart: {
-                        delete: ['dist'],
-                    },
-                },
-            }),
-            new MiniCssExtractPlugin({
-                filename: '[name].[contenthash:8].css',
-            }),
+            },
         ],
-        devServer: {
-            watchFiles: path.join(__dirname, 'src'),
-            port: 3000,
-        },
-    };
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'template.html'),
+            filename: 'index.html',
+        }),
+        new FileManagerPlugin({
+            events: {
+                onStart: {
+                    delete: ['dist'],
+                },
+            },
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash:8].css',
+        }),
+    ],
+    devServer: {
+        watchFiles: path.join(__dirname, 'src'),
+        port: 3000,
+    },
+};
