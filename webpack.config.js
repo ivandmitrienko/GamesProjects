c2d38594d303c9ea6dda223683ef2df2803d19b3
ivandmitrienko <oneblackcrystal@gmail.com>
2023-01-05 21:19:06 +0300

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: path.join(__dirname, 'src', 'index.html'),
             filename: 'index.html',
+            minify: true
         }),
         new FileManagerPlugin({
             events: {
