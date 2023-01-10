9b0228ca79dd4942dfcaf6ff111d1b81c852ee4c
ivandmitrienko <oneblackcrystal@gmail.com>
2023-01-10 23:40:56 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -6,8 +6,40 @@ import './fonts/fonts.scss';
 //     element.textContent = text;
 //     return element;
 //   }
-  
+
 //   document.body.prepend(component('Проект собран на Webpack'));
 
-const inp = document.querySelectorAll('.raiting__item')[3];
-inp.addEventListener('click', ()=> console.log('fuck'));
+const raitings = document.querySelectorAll('.raiting');
+if (raitings.length > 0) {
+    for (let index = 0; index < raitings.length; index++) {
+        setRaiting(raitings[index]);
+    }
+};
+
+function setRaiting(raiting) {
+    const raitingItems = raiting.querySelectorAll('.raiting__item');
+    for (let index = 0; index < raitingItems.length; index++) {
+        const raitingItem = raitingItems[index];
+        console.log(raitingItem);
+        raitingItem.addEventListener("mouseenter", () => {
+            console.log('fff')
+        })
+            //  initRaiting(raiting);
+            //  setRaitingActiveWidth(raitingItem.Value);
+        // )
+
+
+    }
+}
+
+// function initRaiting(raiting) {
+//     const raitingVars = {
+//         raitingValue: raiting.querySelector('.raiting__value'),
+//         raitingActive: raiting.querySelector('.raiting__active'),
+//     }
+// }
+
+// function setRaitingActiveWidth(raitingVars) {
+//     const raitingActiveWidth = raitingVars.raitingValue.innerHTML / 0.05;
+//     raitingVars.raitingActive.style.width = `${raitingActiveWidth}%`
+// }
